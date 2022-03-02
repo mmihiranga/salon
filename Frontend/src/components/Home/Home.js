fad9a1762fe5b3e33eaefcd41bce9ce36c4cb267
Madhura Mihiranga <it19177410@my.sliit.lk>
2022-03-02 13:29:24 +0530

    first

---
+++ Frontend/src/components/Home/Home.js
@@ -0,0 +1,10 @@
+import React, { useState } from 'react'
+
+export const Home = () => {
+    const [more, setMore] = useState(false)
+    return (
+       <div>
+           <div>dsfsdfsdfgdfgdf</div>
+       </div>
+    )
+}
